fb3acff883163c76c85056e8cff76e06c1380920
Saskya Panchaud <saskya.panchaud@epfl.ch>
2024-08-16 12:11:39 +0200

    [fix] filter on sciper ko

--- wip_dashboard.R
+++ wip_dashboard.R
@@ -1,3 +1,7 @@
+# TODO : ajouter details des certificats sur premier onglet, filtre aussi par hostname sur dernier onglet, date d'echeance comprise dans periode pour deuxieme onglet et chnager type d'input pour troisieme onglet
+# DONE : avec un sciper, trouver les certificats associes avec autres personnes responsables
+# MAYBE : info cert + resp sur vue globale ou en ouvrant nouvel onglet
+
 library(here)
 here::i_am("lib.R")
 source(here("lib.R"))
@@ -26,7 +30,10 @@ con_sqlite <- dbConnect(RSQLite::SQLite(), db_path)
 ssl_data <- fromJSON(Search(con_elasticsearch, index = "ssl", size = 10000, raw = TRUE))$hits$hits$"_source" %>% mutate(ipv4 = as.character(ipv4)) %>% mutate(validFrom = as.Date(validFrom), validTo = as.Date(validTo))
 
 # tableau global avec hostname, ip, date_debut, date_fin
-tableau <- ssl_data %>% select(hostname, ipv4, validFrom, validTo) %>% rename(ip = ipv4, date_debut = validFrom, date_fin = validTo)
+# FIXME : comment trier les donnees a la base (pour l'instant sur hostname partout) ?
+tableau <- ssl_data %>% select(hostname, ipv4, validFrom, validTo) %>% rename(ip = ipv4, date_debut = validFrom, date_fin = validTo) %>% arrange(hostname)
+
+# FIXME : trouver un moyen pour afficher les dates autrement mais garder le tri dynamique possible
 
 # FIXME : besoin des donnees de ces sous-tableaux pour afficher les details ?
 subject <- ssl_data$subject
@@ -43,11 +50,14 @@ convertMenuItem <- function(mi,tabName) {
   mi
 }
 
+header <- dashboardHeader(title = "Certificats SSL", dropdownMenuOutput("notifOutput"))
+
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Vue globale", tabName = "listing", icon = icon("list")),
     # TODO : date de debut et de fin pour periode d'echeance et affichage du label
     convertMenuItem(menuItem("Echéances", tabName = "date_filter", icon = icon("circle-exclamation"), DatePicker.shinyInput("date_fin_max", label = "Choisir la date d'échéance maximale :", value = NULL)), tabName = "date_filter"),
+    # TODO : filtre aussi selon hostname
     convertMenuItem(menuItem("Responsables", tabName = "user_filter", icon = icon("info-circle"), numericInput("sciper", "Choisir le sciper d'un responsable :", value = NULL)), tabName = "user_filter")
   )
 )
@@ -74,7 +84,7 @@ body <- dashboardBody(
 )
 
 ui <- dashboardPage(skin = "red",
-  dashboardHeader(title = "Certificats SSL", dropdownMenuOutput("notifOutput")),
+  header,
   sidebar,
   body
 )
@@ -86,6 +96,7 @@ server <- function(input, output) {
   })
 
   # TODO : afficher details des certificats
+  # TODO : nouvel onglet pour ligne selectionnee -> https://stackoverflow.com/questions/45151436/shiny-datatable-popup-data-about-selected-row-in-a-new-window
   output$df_all <- renderDT({
     datatable(tableau, options = list(searching = FALSE), class = 'stripe hover')
   })
@@ -112,9 +123,19 @@ server <- function(input, output) {
   })
 
   output$df_resp <- renderDT({
+    # FIXME : trouver un moyen pour ne pas dupliquer le code
+    sciper <- input$sciper
+    # TODO : trouver un autre type pour sciper car si numeric alors nombre negatif autorise + fleches pour faire +/- 2, 3, ... mais pas de lettres sauf e
+    if (is.na(sciper)) {
+      info_cert <- tableau
+    } else {
+      ips <- dbGetQuery(con_sqlite, sprintf("SELECT User.id_user, User.sciper, Server.id_ip, Server.ip FROM User LEFT JOIN Server_User ON User.id_user = Server_User.id_user LEFT JOIN Server ON Server_User.id_ip = Server.id_ip WHERE sciper = %s;", sciper))
+      info_cert <- tableau %>% filter(ip %in% ips$ip)
+     }
+    
     req(input$df_user_rows_selected) # affichage uniquement si ligne selectionnee
     selected_row <- input$df_user_rows_selected # index de la ligne selectionnee
-    ip <- tableau[selected_row, ]$ip
+    ip <- info_cert[selected_row, ]$ip
     info_user <- dbGetQuery(con_sqlite, sprintf("SELECT sciper, cn, email, rifs_flag, adminit_flag FROM Server LEFT JOIN Server_User ON Server.id_ip = Server_User.id_ip LEFT JOIN User ON Server_User.id_user = User.id_user WHERE Server.ip = '%s';", ip))
     info_user <- info_user %>% rename(nom = cn, rifs = rifs_flag, adminit = adminit_flag) %>% mutate(rifs = ifelse(rifs == 1, "x", ""), adminit = ifelse(adminit == 1, "x", ""))
     # TODO : changer le sytle de l'affichage
