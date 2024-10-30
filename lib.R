6269c3ca2c2d9994b323006ebc65efa44e2e9fe1
Saskya Panchaud <saskya.panchaud@epfl.ch>
2024-10-30 11:20:10 +0100

    [FIX] add R packages

--- lib.R
+++ lib.R
@@ -1,5 +1,5 @@
 # packages install
-packages <- c("Cairo", "DBI", "dotenv", "dplyr", "DT", "elastic", "plyr", "here", "httr", "jsonlite", "kableExtra", "knitr", "log4r", "roperators", "RSQLite", "shiny", "shinydashboard", "shiny.fluent", "stringr", "tidyr", "ragg")
+packages <- c("Cairo", "DBI", "dotenv", "plyr", "dplyr", "DT", "elastic", "here", "httr", "jsonlite", "kableExtra", "knitr", "log4r", "roperators", "RSQLite", "shiny", "shinydashboard", "shiny.fluent", "stringr", "tidyr", "ragg", "ggplot2")
 
 for (p in packages) {
   if (!require(p, character.only = TRUE)) {
