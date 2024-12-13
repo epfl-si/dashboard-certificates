78720a04ae7d303c4b63c223793e3d012a3f428c
saskya <saskya.panchaud@epfl.ch>
2024-12-13 13:53:54 +0100

    [fix] not a good idea

--- ansible/roles/dashboard/templates/reload_data.sh
+++ ansible/roles/dashboard/templates/reload_data.sh
@@ -3,4 +3,6 @@ docker exec cert_dashboard /bin/bash -c "echo \"START :\" >> /tmp/logs.txt && da
 docker exec cert_dashboard /bin/bash -c "echo \"Update cmdb schema :\" >> /tmp/logs.txt && date >> /tmp/logs.txt"
 docker exec cert_dashboard /bin/bash -c "Rscript {{ dashboard_install_path }}/add_cmdb_data.R"
 docker exec cert_dashboard /bin/bash -c "mv {{ dashboard_install_path }}/cmdb_temp.sqlite {{ dashboard_install_path }}/cmdb.sqlite"
+docker exec cert_dashboard /bin/bash -c "echo \"Reload dashboard :\" >> /tmp/logs.txt && date >> /tmp/logs.txt"
+docker exec cert_dashboard /bin/bash -c "Rscript {{ dashboard_install_path }}/dashboard.R"
 docker exec cert_dashboard /bin/bash -c "echo \"END :\" >> /tmp/logs.txt && date >> /tmp/logs.txt"
